07d03d644bf45748c282a09cf33a5e55ed948553
meliheknciii <melihekinciofficial@gmail.com>
2024-04-20 10:18:36 +0300

    Sunset Project
    Kotlin dilini kullanarak ilk projemi yaptım.
    animasyon eklemeyi  ve font yapısını değiştirmeyi öğrendim.

---
+++ SunsetProject/app/src/main/java/com/melihekinci/sunsetproject/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.melihekinci.sunsetproject
+
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.melihekinci.sunsetproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+       val  view = binding.root
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
